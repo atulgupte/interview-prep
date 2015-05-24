489975b5245b49f720c6849a0b52eb687dc9938b
Atul Gupte <agupte@zynga.com>
2015-05-23 22:16:41 -0700

    Problem: Find square root of a number without using inbuilt function

--- InterviewPrep/src/find_sqrt.java
+++ InterviewPrep/src/find_sqrt.java
@@ -1,9 +1,29 @@
 
 public class find_sqrt {
 
+	public static double sqrt(double number, double epsilon) {
+	
+		double left = 0.0;
+		double right = number;
+		double mid = 0.0;
+		
+		while (left + epsilon < right) {
+			mid = (left + right)/2.0;
+			if (mid*mid > number) {
+				right = mid;
+			} else {
+				left = mid;
+			}
+		}
+		
+		return left;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		double number = 49.0;
+		double epsilon = 0.000000001;
+		double result = sqrt(number, epsilon);
+		System.out.println("Square root of " + number + " is: " + result);
 	}
 
 }
